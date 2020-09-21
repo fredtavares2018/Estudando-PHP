540c6c2d6850bd2613e47b6074448dc74e840d06
fredtavares2018 <nordesttino@hotmail.com>
2020-09-21 01:29:13 -0300

    arquivos sessao ok

--- usandoSessao/teste_login.php
+++ usandoSessao/teste_login.php
@@ -5,30 +5,28 @@ session_start();
 
 // recebendo o valor vindo da página login.php
 // verificador ternário
-$verificando = isset($_POST['login']) ? $_POST['login']:"";
+$verificando_login = isset($_POST['login']) ? $_POST['login'] : "";
+$verificando_senha = isset($_POST['senha']) ? $_POST['senha'] : "";
 
-if(($verificando == 0) or (empty($verificando))){
+if ((empty($verificando_login)) or (empty($verificando_senha))) {
+    $_SESSION['logar'] = "0";
+    header("location: logado.php");
+}else {
     // aqui entra toda parte do mysqli SELECT
-    // vai buscar no Banco de Dados o $verificando
-    // agora mandamos contar as "linhas" 
+    // vai buscar no Banco de Dados o $verificando_login e $verificando_senha
+    // agora mandamos contar as "linhas" $num_rows
 
-    //$result = "SELECT * FROM table1 WHERE login = $verificando";
+    //$result = "SELECT * FROM table1 WHERE (login = $verificando_login) and (senha = $verificando_senha) ";
     //$verificando_result = mysqli_query($conn, $result);
     //$num_rows = mysqli_num_rows($verificando_result);
 
-    //if($num_rows > 0){
-        //$_SESSION['logar'] = "101";
+    //se retornar $num_rows = 1 é porque só existe 1 registro e está ok
+
+    //if($num_rows == 1){
+    //$recebendo_array = mysqli_fetch_array($verificando_result);
+    //$_SESSION['logar'] = $recebendo_array['email'];
+    //header("location: logado.php");
     //}else{
-        $_SESSION['logar'] = "0";
-        header("location: logado.php");
-    //}
-}elseif($verificando == 1){
     $_SESSION['logar'] = "1";
     header("location: logado.php");
-}else{
-    $_SESSION['logar'] = $verificando;
-    header("location: logado.php");
 }
-
-
-?>
